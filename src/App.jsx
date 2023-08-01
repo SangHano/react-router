532b0c244f2fb7b6f2424828537e2f72968e4e4d
SangHano <sangchulhan.mike@gmail.com>
2023-08-01 16:38:34 -0400

    added component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route, Link } from "react-router-dom";
 import Home from "./components/Home"
 import Blue from "./components/Blue"
 import Red from "./components/Red"
+import Green from "./components/Green"
 import './App.css'
 
 const App = () => {
@@ -13,12 +14,14 @@ const App = () => {
           <Link id="homeLink" to="/">Home</Link>
           <Link id="blueLink" to="/blue">Daily Life</Link>
           <Link id="redLink" to="/red">Not Done YET!!!!!</Link>
+          <Link id="greenLink" to ="/green">Not Dont YET!!!</Link>
         </nav>
 
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/blue" element={<Blue/>} />
           <Route path="/red" element={<Red/>} />
+          <Route path="/green" element={<Green/>}/>
         </Routes>
       </div>
     </>
---
+++ src/components/Green.jsx
@@ -0,0 +1,8 @@
+const Green =()=>{
+    return(
+        <div className="green">
+          <h1>hi</h1>
+        </div>
+    )
+}
+export default Green
